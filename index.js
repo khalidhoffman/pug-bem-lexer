9217f757d289ec1ec921cdae920a0977a9e4338c
Khalid Hoffman <khalidhoffman@gmail.com>
2016-09-30 10:55:07 -0400

    ensures only first block class is used for descendant classes

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ module.exports = function () {
     if (lastToken) {
         switch(lastToken.type){
             case 'class':
-                if(lastToken.val.match(/^[a-zA-Z]/)){
+                if(lastToken.val.match(/^[a-zA-Z]/) && !(currentBemBlock && currentBemBlock.line == lastToken.line)){
                     this.bemBlocks.push(lastToken);
                 } else if (currentBemBlock && lastToken.val.match(/^\-/)){
                     this.tokens[lastTokenIndex].val = lastToken.val.replace(/^\-/, currentBemBlock.val + '--');
                 } else if(currentBemBlock && lastToken.val.match(/^\_/)){
                     this.tokens[lastTokenIndex].val = lastToken.val.replace(/^\_/, currentBemBlock.val + '__');
-                };
+                }
                 break;
             case 'outdent':
                 if(currentBemBlock && this.colno <= currentBemBlock.col) {
-                    var poppedToken = this.bemBlocks.pop();
+                    this.bemBlocks.pop();
                 }
                 break;
             default:
                 break;
         }
     }
-}
+};
