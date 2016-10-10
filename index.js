1b08a77f5026723b83c6897d6290d8a53fdacdf4
Khalid Hoffman <khalidhoffman@gmail.com>
2016-10-10 14:00:40 -0400

    adds better check for secondary classes

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ module.exports = function () {
         switch(currentToken.type){
             case 'class':
                 if(currentToken.val.match(/^[a-zA-Z]/) && !(currentBEMBlock && currentBEMBlock.line == currentToken.line)){
-                    this.bemBlocks.push(currentToken);
+                    var prevToken = this.tokens[this.tokens.length -2];
+                    if (prevToken && prevToken.type == 'class'){
+                        // ignore secondary classes
+                    } else {
+                        this.bemBlocks.push(currentToken);
+                    }
                 } else if (currentBEMBlock && currentToken.val.match(/^\-/)){
                     this.tokens[currentTokenIndex].val = currentToken.val.replace(/^\-\-?/, currentBEMBlock.val + '--');
                 } else if(currentBEMBlock && currentToken.val.match(/^\_/)){
